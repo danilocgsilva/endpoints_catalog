a7a8bba118150e0c09c9965317ce78478b6176f2
Danilo Carlos <danilo.goes_leega@totalexpress.com.br>
2024-08-28 20:12:58 +0000

    Custom Exception corrected

--- src/NoMigrationsLeftException.php
+++ src/NoMigrationsLeftException.php
@@ -5,8 +5,15 @@ declare(strict_types=1);
 namespace Danilocgsilva\EndpointsCatalog;
 
 use Exception;
+use Throwable;
 
 class NoMigrationsLeftException extends Exception
 {
-    protected string $message = "There's no migrations left.";
+    public function __construct(
+        string $message = "No migrations left.", 
+        int $code = 0, 
+        Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
 }
