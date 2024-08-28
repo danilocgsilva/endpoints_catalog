888422ee911b16a0d84c0c764e91dc00b0d9ac74
Danilo Carlos <danilo.goes_leega@totalexpress.com.br>
2024-08-28 12:11:52 +0000

    Dynamically knows which rollback class to provide

--- src/Migrations/MigrationManager.php
+++ src/Migrations/MigrationManager.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Danilocgsilva\EndpointsCatalog\Migrations;
 
+use PDO;
+use PDOException;
+
 class MigrationManager
 {
     public function getNextMigrationClass(): string
@@ -11,8 +14,22 @@ class MigrationManager
         return "Danilocgsilva\EndpointsCatalog\Migrations\Apply\M02_MetaTable";
     }
 
-    public function getPreviouseMigrationClass(): string
+    public function getPreviouseMigrationClass(PDO $pdo): string
     {
-        return "Danilocgsilva\EndpointsCatalog\Migrations\Rollback\M02_MetaTableRollback";
+        if ($this->hasTable('migrations', $pdo)) {
+            return "Danilocgsilva\EndpointsCatalog\Migrations\Rollback\M02_MetaTableRollback";
+        }
+        return "Danilocgsilva\EndpointsCatalog\Migrations\Rollback\M01_Rollback";
+    }
+
+    private function hasTable(string $table, PDO $pdo)
+    {
+        try {
+            $pdo->query("SELECT 1 FROM {$table} LIMIT 1");
+        } catch (PDOException $e) {
+            return false;
+        }
+    
+        return true;
     }
 }
