e6799f0ba41ac13a55c3ea0364e19b0d847d1885
Danilo Carlos <danilo.goes_leega@totalexpress.com.br>
2024-09-05 12:42:22 +0000

    No migrations left after full migration

--- src/Migrations/Manager.php
+++ src/Migrations/Manager.php
@@ -6,16 +6,24 @@ namespace Danilocgsilva\EndpointsCatalog\Migrations;
 
 use Danilocgsilva\EndpointsCatalog\Migrations\Migrations\{M01_Apply, M02_Platforms};
 use Danilocgsilva\EndpointsCatalog\Models\{Path, Dns, DnsPath};
+use Danilocgsilva\EndpointsCatalog\Models\Platform;
 use PDO;
 
 class Manager
 {
+    /**
+     * @var array<string>
+     */
+    private array $tables;
+    
     public function __construct(private string $databaseName, private PDO $pdo) {}
     
     public function getNextMigration(): MigrationInterface
     {
         if ($this->haveTablesFirstMigration()) {
-            return new M02_Platforms();
+            if (!in_array(Platform::TABLENAME, $this->tables)) {
+                return new M02_Platforms();
+            }
         }
         throw new NoMigrationsLeftException();
     }
@@ -40,15 +48,22 @@ class Manager
      */
     private function listTables(): array
     {
+        if (isset($this->tables)) {
+            return $this->tables;
+        }
+
         $query = sprintf("USE %s;", $this->databaseName);
-        $query .= PHP_EOL . "SHOW TABLES;";
+        $this->pdo->prepare($query)->execute();
+
+        $query = "SHOW TABLES;";
         $preResults = $this->pdo->prepare($query);
         $preResults->setFetchMode(PDO::FETCH_NUM);
         $preResults->execute();
-        $tables = [];
+        $this->tables = [];
         while ($row = $preResults->fetch()) {
-            $tables[] = $row[0];
+            $this->tables[] = $row[0];
         }
-        return $tables;
+
+        return $this->tables;
     }
 }
