2e8f50351ce807a977a356c65ad1ce957669a8c1
Danilo Carlos <danilo.goes_leega@totalexpress.com.br>
2024-09-05 14:32:54 +0000

    Dns foreign key for platform

--- src/Migrations/Migrations/M02_Platforms.php
+++ src/Migrations/Migrations/M02_Platforms.php
@@ -5,13 +5,29 @@ declare(strict_types=1);
 namespace Danilocgsilva\EndpointsCatalog\Migrations\Migrations;
 
 use Danilocgsilva\ClassToSqlSchemaScript\FieldScriptSpitter;
+use Danilocgsilva\ClassToSqlSchemaScript\ForeignKeyScriptSpitter;
 use Danilocgsilva\ClassToSqlSchemaScript\TableScriptSpitter;
 use Danilocgsilva\EndpointsCatalog\Migrations\MigrationInterface;
+use Danilocgsilva\EndpointsCatalog\Models\Dns;
 use Danilocgsilva\EndpointsCatalog\Models\Platform;
 
 class M02_Platforms implements MigrationInterface
 {
     public function getString(): string
+    {
+        $newPlatformTableString = $this->getNewTableString();
+        $alterDnsString = $this->alterDnsTable();
+        $foreignKey = $this->addForeignKeyToDns();
+
+        return $newPlatformTableString . PHP_EOL . $alterDnsString . PHP_EOL . $foreignKey;
+    }
+
+    public function getRollbackString(): string
+    {
+        return sprintf("DROP TABLE %s;", Platform::TABLENAME);
+    }
+
+    private function getNewTableString(): string
     {
         return (new TableScriptSpitter(Platform::TABLENAME))
             ->addField(
@@ -34,8 +50,19 @@ class M02_Platforms implements MigrationInterface
             ->getScript();
     }
 
-    public function getRollbackString(): string
+    private function alterDnsTable(): string
     {
-        return sprintf("DROP TABLE %s;", Platform::TABLENAME);
+        return sprintf("ALTER TABLE %s ADD COLUMN platform_id INT NOT NULL UNSIGNED;", Dns::TABLENAME);
+    }
+
+    private function addForeignKeyToDns(): string
+    {
+        return (new ForeignKeyScriptSpitter())
+            ->setConstraintName('dns_platform_id_constraint')
+            ->setForeignKey('platform_id')
+            ->setTable(Dns::TABLENAME)
+            ->setForeignTable(Platform::TABLENAME)
+            ->setTableForeignkey('id')
+            ->getScript();
     }
 }
