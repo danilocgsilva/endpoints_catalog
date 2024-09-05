e082ff1c349d0cb5486a28878aedad3e3a1ed223
Danilo Carlos <danilo.goes_leega@totalexpress.com.br>
2024-09-05 19:15:37 +0000

    dns path repository: find

--- src/Models/DnsPath.php
+++ src/Models/DnsPath.php
@@ -16,6 +16,8 @@ final class DnsPath
 
     public readonly int $dns_id;
 
+    public readonly int $id;
+
     public function setDnsId(int $dns_id): self
     {
         $this->dns_id = $dns_id;
@@ -25,6 +27,12 @@ final class DnsPath
     public function setPathId(int $path_id): self
     {
         $this->path_id = $path_id;
+        return $this;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
         return $this;
     }
 }
--- src/Repositories/DnsPathRepository.php
+++ src/Repositories/DnsPathRepository.php
@@ -76,6 +76,23 @@ class DnsPathRepository extends AbstractRepository implements BaseRepositoryInte
         }
     }
 
+    public function findByDnsIdAndPathId(int $dnsId, int $pathId): DnsPath
+    {
+        $query = sprintf("SELECT id, path_id, dns_id FROM %s WHERE path_id = :path_id AND dns_id = :dns_id;", self::MODEL::TABLENAME);
+        $preResults = $this->pdo->prepare($query);
+        $preResults->execute([
+            ':path_id' => $dnsId,
+            ':dns_id' => $pathId
+        ]);
+        $preResults->setFetchMode(PDO::FETCH_ASSOC);
+        $row = $preResults->fetch();
+
+        return (new DnsPath())
+            ->setId($row['id'])
+            ->setDnsId($row['dns_id'])
+            ->setPathId($row['path_id']);
+    }
+    
     public function delete(int $id): void
     {
         $this->pdo->prepare(
